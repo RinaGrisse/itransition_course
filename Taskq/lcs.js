3ba1b47a0b02103fc0527e9627c114a18148036f
RinaGrisse <golovina.kate05@gmail.com>
2025-09-24 04:21:23 +0300

    third part

--- Taskq/lcs.js
+++ Taskq/lcs.js
@@ -4,13 +4,15 @@ if (a.length > 2) {
     if (a.length == 3) r = a[2]
     else {
         let s = a[2]
-        a.map(x => { if (x.length < s.length) s = x })
-        for (let l = s.length; l > 0 && !r; l--)
-            for (let j = 0; j <= s.length - l && !r; j++) {
+        for (let i = 3; i < a.length; i++)if (a[i].length < s.length) s = a[i]
+        for (let l = s.length; l > 0; l--) {
+            for (let j = 0; j <= s.length - l; j++) {
                 let t = s.substr(j, l), f = 1
                 for (let k = 2; k < a.length; k++)if (a[k].indexOf(t) < 0) { f = 0; break }
-                f && (r = t)
+                if (f && t.length > r.length) r = t
             }
+            if (r) break
+        }
     }
 }
 console.log(r)
